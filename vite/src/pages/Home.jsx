72a2d9703ee88e4b7285ad7cee913a053883159b
sevval <sevvalislekter.328@gmail.com>
2025-02-08 17:27:56 +0300

    e-ticaret

---
+++ vite/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductList from '../component/ProductList';
+
+function Home() {
+    return (
+        <div>
+            <ProductList />
+        </div>
+    )
+}
+
+export default Home
